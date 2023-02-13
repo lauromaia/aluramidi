89b89cdb892758e26d8d030d554cf99ce0a4c408
laurodrigues <laurodrigues29@gmail.com>
2023-02-13 18:39:54 -0300

    fim do primeiro curso

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorDeAudio){
+    const elemento = document.querySelector(seletorDeAudio);
+
+    if (elemento && elemento.localName === 'audio'){
+        elemento.play();
+    }
+    else {
+        console.log('Elemento ou seletor inválido')
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
 
 listaDeTeclas.forEach(tecla => {
     const instrumento = tecla.classList[1];
     const idAuto = `#som_${instrumento}`;
     tecla.onclick = function () {
         tocaSom(idAuto);
+    }
+
+    tecla.onkeydown = function(evento){
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
     }
 
 })
