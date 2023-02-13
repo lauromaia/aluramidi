888aba046682054fbaa0e781c4157745781c906c
laurodrigues <laurodrigues29@gmail.com>
2023-02-13 15:21:25 -0300

    implementação dos sons as teclas

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-alert('ola mundo');
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
+
+listaDeTeclas.forEach(tecla => {
+    const instrumento = tecla.classList[1];
+    const idAuto = `#som_${instrumento}`;
+    tecla.onclick = function () {
+        tocaSom(idAuto);
+    }
+
+})
+
+
